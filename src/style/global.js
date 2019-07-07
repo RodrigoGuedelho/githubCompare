676f1425e801d690810c745d3dd73e1f42cb7730
Rodrigo Guedelho <rodrigohmjj97@hotmail.com>
2019-07-07 11:03:25 -0300

    Criado arquivo de estilização global da aplicação;

---
+++ src/style/global.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  } 
+  body {
+    background: #9B65E6;
+    text-rendering: optimizeLegibility !important;
+    -webkit-font-smoothing: antialiased !important;
+    font-family: sans-serif;
+  }
+`;
+
+export default GlobalStyle;
