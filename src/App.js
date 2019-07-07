1deb9dbf956e862523c9f431a01f1826d0cfa9ba
Rodrigo Guedelho <rodrigohmjj97@hotmail.com>
2019-07-07 10:57:58 -0300

    Configurações de iniciais do projeto;

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+import GlobalStyle from "./style/global";
+import Main from "./pages/Main";
+
+const App = () => (
+  <Fragment>
+    <GlobalStyle />
+    <Main />
+  </Fragment>
+);
+
+export default App;
