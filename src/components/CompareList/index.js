0b2c15d6f0068796bff6a66e49b67b5cfca19658
Rodrigo Guedelho <rodrigohmjj97@hotmail.com>
2019-07-07 11:00:21 -0300

    Foi criado componente compareList e seu arquivo de estilização;

---
+++ src/components/CompareList/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Container, Repository } from "./style";
+
+const CompareList = ({ repositories }) => (
+  <Container>
+    {repositories.map(repository => (
+      <Repository key={repository.id}>
+        <header>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <strong>{repository.name}</strong>
+          <small>{repository.owner.login}</small>
+        </header>
+
+        <ul>
+          <li>
+            {repository.stargazers_count} <small>start</small>
+          </li>
+          <li>
+            {repository.forks_count} <small>forks</small>
+          </li>
+          <li>
+            {repository.open_issues_count} <small>issues</small>
+          </li>
+          <li>
+            {repository.pushed_at} <small>last commit</small>
+          </li>
+        </ul>
+      </Repository>
+    ))}
+  </Container>
+);
+
+export default CompareList;
